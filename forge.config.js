f69eb1f4546fa715b1b3d1777cd4288d041d663c
oazambranoa <oazambranoa@unal.edu.co>
2024-06-13 13:32:32 -0500

    Forge config

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,8 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    out: 'out',
+    prune: true,
     asarUnpack: "**/libs/addon/**/*",
   },
   rebuildConfig: {},
